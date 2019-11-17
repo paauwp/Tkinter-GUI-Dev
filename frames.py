a84ce568a7e595080aaec1e6ae03c9a6aeb1cf3e
Pieter Paauw <39484121+paauwp@users.noreply.github.com>
2019-11-17 16:36:32 +0100

    Add files via upload

---
+++ frames.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from tkinter import ttk 
+
+root = tk.Tk()
+root.geometry("600x400")
+
+main = ttk.Frame(root)
+main.pack(side="left", fill="both", expand=True)
+
+tk.Label(main, text="Label Top", bg="red").pack(side="top", expand=True, fill="both")
+tk.Label(main, text="Label Top", bg="red").pack(side="top", expand=True, fill="both")
+tk.Label(root, text="Label Left", bg="green").pack(side="left", expand=True, fill="both")
+
+root.mainloop()
